c1a4237738cc629eabcf93eb782541be19090f66
zuzananagyova <97087015+zuzananagyova@users.noreply.github.com>
2025-08-22 09:05:46 +0200

    task 1 test

---
+++ task1techtestzuzka/task1techtestzuzka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1ZuzkaConsoleApp1
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("#BestVisma");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Best");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Visma");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
